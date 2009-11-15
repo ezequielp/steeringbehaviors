bc53e876abf5fd0902839996a366f44b3b916259
ezequiel.pozzo <ezequiel.pozzo@cdc9dd38-cc87-11de-a70d-3b9dd4243769>
2009-11-15 17:47:31 +0000

    Basically the same as the reference, with changed names to have a similar interface wxpyhton has (and I liked). But the code has comments that hint on what I'm going to add next, that is different than the basic Event Manager.

---
+++ steeringbehaviors/src/Mediator/EventManager.py
@@ -0,0 +1,41 @@
+'''
+Created on 14/11/2009
+
+@author: Ezequiel N. Pozzo
+'''
+
+class EventManager(object):
+    '''This object will mediate most communication between model, view and controller
+    TODO:
+    
+    '''
+
+
+    def __init__(self):
+        '''
+        WeakKeyDictionary allows the Garbage collector to delete the listener objects if 
+        all other references are lost.
+        '''
+        from weakref import WeakKeyDictionary
+        self.listeners = WeakKeyDictionary()
+        
+    def bind(self, listener, event_type=None):
+        '''
+        Registers an event listener. 
+        @listener Object who will receive events. Must implement notify function.
+        @event_type The type of events the listener will hear, default: Hears all.
+        
+        TODO: implement listening to type of events. Assert notify in listener.
+        '''
+        self.listeners[listener]=True
+        
+    def unbind(self, listener):
+        if listener in self.listeners.keys():
+            del self.listeners[listener]
+            
+    def post(self, event):
+        '''
+        Sends event to all listeners
+        '''
+        for listener in self.listeners.keys():
+            listener.notify(event)
