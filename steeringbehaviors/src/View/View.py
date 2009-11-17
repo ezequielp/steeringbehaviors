01abdbee33f362d99d8489ca828353edab0b9955
ezequiel.pozzo <ezequiel.pozzo@cdc9dd38-cc87-11de-a70d-3b9dd4243769>
2009-11-17 00:36:09 +0000

    I'm feeling lucky implementation of pygame view (didn't test it!)

--- steeringbehaviors/src/View/View.py
+++ steeringbehaviors/src/View/View.py
@@ -6,6 +6,8 @@ Edited by JuanPi Carbajal
 '''
 from __future__ import division
 import numpy as np
+from Tools import real2pix as rp
+from View import config
 
 class View(object):
     '''
@@ -17,7 +19,13 @@ class View(object):
     
     def update(self):
         pass
-        
+       
+    def add_new_entity(self, model_entity):
+        ''' 
+        Ugly hack that will be solved when using events 
+        TODO: implement as a event handler function add_new_entity(self, event)
+        '''
+        assert False, "Not implemented"
 
 class View2D(View):
     '''
@@ -28,33 +36,78 @@ class View2D(View):
         View.__init__(self, Model)
        
         # JPi: How do we define private variables and methods?
-        self._sprites=np.array([])
-        self._project=rp.Transformation()
+        self.__sprites=np.array([])
+        self.__project=rp.Transformation()
     
     def update(self):
         View.update(self)
         # Here sprites are updated. How? Idea: apply the transformation
         # self._project.transformation(Model.actors) or something like that.
+        # 
         pass
     
     def set_transform(self,move=np.array([0,0]), rotate=np.array([0]),scale=np.array([1,1])):
-        # Works only in 2D for the moment                                 
-                    
-        # Rotation
-        R = rotv(np.array([0,0,1]),rotate[0])
-        # Scale
-        S = np.diag(np.concatenate([scale,[1]]))
+        self.__project.set_transform(move, rotate, scale)
         
-        # Translation
-        A = zeros([3,3])
-        for i in xrange(0,2):
-            A[i,-1]=move[i]*scale[i]
 
-        self.T=dot(R,S)+A  
-                  
 class PygameViewer(View2D):
-'''
-A class rendering the actors of the Model into a Pygame window
-'''        
-    # JPi: Here is Ezequiels Magic!
+    '''
+    A class rendering the actors of the Model into a Pygame window
+    '''        
+    import pygame
+    from pygame.sprite import Sprite as SpriteParent
     
+    class Sprite(SpriteParent):
+        def __init__(self, model_entity_position):
+            '''
+            @model_entity_position: the position of the model object. Use [position] to get reference!!!
+            '''
+            pygame=PygameViewer.pygame
+            self.SpriteParent.__init__(self)
+            self.model_position=model_entity_position
+            
+            self.rect=pygame.Rect(self.__project(model_entity_position), (0,0))
+            
+            '''
+            TODO: implement better and more versatile method to set sprite image
+            '''
+            simple_sprite=pygame.Surface((6,6))
+            simple_sprite.fill((255,255,255))
+            simple_sprite.set_colorkey((255,255,255), pygame.RLEACCEL)
+            self.image=simple_sprite
+        
+            pygame.draw.circle(self.image, (0,0,0), (3,3), 3.0)
+            
+            
+        def update(self):
+            self.rect.center=self.__project.transform(self.model_position[0])
+        
+        
+    def __init__(self, Model):
+        View2D.__init__(self, Model)
+        pygame=self.pygame
+        pygame.init()
+        self.Sprite.__project=self.__project
+        self.sprites=self.pygame.sprite.RenderUpdates()
+        
+        '''
+        Starts a simple black screen.
+        TODO: improve to be configurable
+        '''
+        self.screen=pygame.display.set_mode(config.screen_size)
+        background = pygame.Surface(self.screen.get_size())
+        background = background.convert()
+        background.fill((250, 250, 250))
+        self.background=background
+        self.screen.blit(background, (0,0))
+        pygame.display.flip()
+        
+    def update(self):
+        self.sprites.clear(self.screen, self.background)
+        self.sprites.update()
+        self.pygame.display.update(self.sprites.draw())
+        
+    def add_new_entity(self, model_entity):
+        self.sprites.add(self.Sprite(self, [model_entity.getPosition()]))
+
+        
