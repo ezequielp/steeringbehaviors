0e3c221eba6b3dc0e3a91220eb769a20710bf164
ezequiel.pozzo <ezequiel.pozzo@cdc9dd38-cc87-11de-a70d-3b9dd4243769>
2009-11-24 22:41:53 +0000

    Started Drag and Drop App

---
+++ steeringbehaviors/src/Controller/MiscControllers.py
@@ -0,0 +1,48 @@
+'''
+Created on 24/11/2009
+
+@author: Ezequiel N. Pozzo
+'''
+
+class CPUSpinner(object):
+    '''
+    An abstract clock object that generates ticks at a given FPS.
+    '''
+
+
+    def __init__(self, fps, event_handler):
+        '''
+        fps is the number of ticks per seconds to emit
+        '''
+        self.fps=fps
+        self.event_handler=event_handler
+        self.TICK=event_handler.new_event_type()
+        
+    def run(self):
+        assert False, "Abstract Class, use specific implementation instead"
+        
+        
+class PygCPUSpinner(CPUSpinner):
+    '''
+    Specific CPUSpinner implementation using pygame
+    '''    
+    def __init__(self, fps, event_handler):
+        CPUSpinner.__init__(self, fps, event_handler)
+        import pygame
+        self.pygame=pygame
+        self.pygame.init()
+        self.clock=self.pygame.time.Clock()
+        
+        
+        
+    def run(self):
+        eh=self.event_handler
+        TICK=self.TICK
+        self.running=True
+        while self.running:
+            self.clock.tick(self.fps)
+            eh.post({'Type': TICK})
+            
+    def stop(self):
+        self.running=False
+        
--- steeringbehaviors/src/drag_and_drop.py
+++ steeringbehaviors/src/drag_and_drop.py
@@ -2,12 +2,61 @@ from View.View import PygameViewer
 from Model.Model import PhysicsModel
 from Controller.MouseController import PygameMouseController
 from Mediator.EventManager import EventManager
+from Controller.MiscControllers import PygCPUSpinner
 
-class App():
-	event_handler=EventManager()
+FPS=10 #Same FPS for all for the moment
+
+class DragAndDropApp():
 	
+	def __init__(self, event_handler, world, screen, mouse, spinner):
+		self.event_handler=event_handler
+		self.world=world
+		self.screen=screen
+		self.mouse=mouse
+		self.spinner=spinner
+		
+		event_handler.bind(self.on_mouse_down, mouse.MOUSE_BTN1_DOWN)
+		event_handler.bind(self.on_mouse_move, mouse.MOUSE_MOVE)
+		event_handler.bind(self.on_mouse_left_up, mouse.MOUSE_BTN3_UP) #Left click ends app
+		
+		for listener_obj in [self.mouse, self.world, self.screen ]:
+			event_handler.bind(listener_obj.on_update, self.spinner.TICK)  
+			
+			
+		self.grabbed=None
+		
+	def on_mouse_down(self, event):
+		entity=screen.get_entity_at(event.pos)
+		if entity!=None:
+			self.grabbed=entity
+			self.model.grab_entity(entity)
+			
+	def on_mouse_up(self, event):
+		if self.grabbed!=None:
+			self.model.drop_entity(self.grabbed)
+			
+	def on_mouse_move(self, event):
+		if self.grabbed!=None:
+			self.model.move_entity(self.grabbed, self.view.get_world_position(event.pos))
+			
+	def on_mouse_left_up(self):
+		self.spinner.stop()
+		
+	def Run(self):
+		'''
+		App will not return from Run until the spinner is stopped, so be sure to bind an event to 
+		self.spinner.stop() 
+		'''
+		
+		self.spinner.run()
+	
+if __name__ == '__main__':
+	event_handler=EventManager()
 	world=PhysicsModel()
 	screen=PygameViewer(world)
 	mouse=PygameMouseController(event_handler)
-	def __init__(self):
+	spinner=PygCPUSpinner(FPS)	
+	
+	python_app=DragAndDropApp(event_handler, world, screen, mouse, spinner)	
+			
 		
