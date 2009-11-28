df68363474e7453b9c1a8e13848718bdc14c7389
ezequiel.pozzo <ezequiel.pozzo@cdc9dd38-cc87-11de-a70d-3b9dd4243769>
2009-11-28 20:26:13 +0000

    Started croshair controller

---
+++ steeringbehaviors/src/Controller/Crosshair.py
@@ -0,0 +1,17 @@
+'''
+Created on 28/11/2009
+
+@author: Ezequiel N. Pozzo
+'''
+
+class Crosshair(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
