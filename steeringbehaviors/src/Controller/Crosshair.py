83593c6563c2973c379465213a0071ee4452b864
ezequiel.pozzo <ezequiel.pozzo@cdc9dd38-cc87-11de-a70d-3b9dd4243769>
2009-11-28 21:00:02 +0000

    More Crosshair

--- steeringbehaviors/src/Controller/Crosshair.py
+++ steeringbehaviors/src/Controller/Crosshair.py
@@ -18,11 +18,16 @@ class Crosshair(Controller):
         Controller.__init__(self, event_handler)
         self.view=view
         self.world=world_model
-        
-        #registers sprite
-        view.add_entity()
+        #registers sprite on model and view. Grabs it to disallow physics modification
+        ch_id=world_model.add_entity((0,0), (0,0))
+        world_model.grab_entity(ch_id)
+        view.add_sprite(ch_id)
+        self.ch_id
         
     #Callbacks for mouse controller
     def mouse_move_cb(self, event):
         position=event['Pos']
+        view_transform=self.view.get_world_position
+        self.world.
+        
         
