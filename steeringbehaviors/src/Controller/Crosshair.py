a1fedd5f5c7acb8f26d51f3867aa3844487ad1cc
ezequiel.pozzo <ezequiel.pozzo@cdc9dd38-cc87-11de-a70d-3b9dd4243769>
2009-11-28 20:41:20 +0000

    More Crosshair

--- steeringbehaviors/src/Controller/Crosshair.py
+++ steeringbehaviors/src/Controller/Crosshair.py
@@ -3,15 +3,26 @@ Created on 28/11/2009
 
 @author: Ezequiel N. Pozzo
 '''
+from Controller import Controller
 
-class Crosshair(object):
+class Crosshair(Controller):
     '''
-    classdocs
+    A simple 
     '''
 
 
-    def __init__(selfparams):
+    def __init__(self, view, world_model, event_handler):
         '''
         Constructor
         '''
+        Controller.__init__(self, event_handler)
+        self.view=view
+        self.world=world_model
+        
+        #registers sprite
+        view.add_entity()
+        
+    #Callbacks for mouse controller
+    def mouse_move_cb(self, event):
+        position=event['Pos']
         
