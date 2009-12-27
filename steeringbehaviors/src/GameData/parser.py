c2fc4b7cb39ee0d15f8f0cc5304ca4406ec3aec3
ajuanpi <ajuanpi@cdc9dd38-cc87-11de-a70d-3b9dd4243769>
2009-12-27 22:36:41 +0000

    First version

--- steeringbehaviors/src/GameData/parser.py
+++ steeringbehaviors/src/GameData/parser.py
@@ -14,7 +14,7 @@ class Dispatcher:
         
         method = getattr(self, mname, None)
         
-        # If the method is there use it, if nt use the default
+        # If the method is there use it, if not use the default
         if callable(method): args = ()
         else:
             method = getattr(self, dname, None)
@@ -30,25 +30,128 @@ class Dispatcher:
     def endElement(self, name):
         self.dispatch('end', name)
 
-class SVGParser(Dispatcher, ContentHandler): 
+class SVGParser(Dispatcher, ContentHandler):
     def __init__(self):
         ContentHandler.__init__(self)
-        self.layers_count = 0
+        self.LAYER='inkscape:groupmode'
+        self.LABEL='inkscape:label'
+        self.LINK='xlink:href'
         
+        self.__current=dict()
+        
+        self.view=dict()
+        
+        #self.layers_count = 0
+    
     def startG(self, attrs):
-        if 'layer' in attrs['id']:
-            self.layers_count +=1
-            print attrs.keys()
+        '''
+            Dispatches the different ethods for the different layers
+        '''
+        if  self.getValue(attrs,self.LAYER):
+            self.dispatch('start',self.getValue(attrs,self.LABEL),attrs)
+            
     def endG(self):
-        pass
+        if self.__current:
+            self.dispatch('end',self.__current["layer"])
+            
+    def startView(self,attrs):
+#        self.__current.update(attrs)
+        self.__current.update(layer="View")
+        print "Parsing View"
+    
+    def endView(self):
+        self.__current.clear()
+        print "Ending View"
         
+    def startImage(self,attrs):
+        if self.__current['layer'] == "View":
+            self.view["avatar"]=attrs[self.LINK][7:] # Erase file://
+            
+        
+    def startController(self,attrs):
+        self.__current.update(attrs)
+        self.__current.update(layer="Controller")
+        print "Parsing Controller"
+        
+    def endController(self):
+        self.__current.clear()
+        print "Ending Controller"
+
+    def startModel(self,attrs):
+        self.__current.update(attrs)
+        self.__current.update(layer="Model")
+        print "Parsing Model"
+                            
+    def endModel(self):
+        self.__current.clear()
+        print "Ending Model"
+
     def startDefault(self,attrs):
         pass
 
     def endDefault(self):
         pass
-    
-layers=SVGParser()
-parse('Player_demo.svg',layers)
+        
+    def getValue(self,attrs,attrname):
+        # Search for an attribute name and return value
+        if attrname in attrs:
+            return attrs[attrname]
+        else:
+            return None 
+         
+class Player(object):
+    import pygame
+    import sys
+        
+    def __init__(self,player_file):
+        self.data=SVGParser()
+        parse(player_file,self.data)
+        
+        
+    # Subclass pygame.sprite.Sprite
+    class Avatar(pygame.sprite.Sprite):
 
-print 'There are ', layers.layers_count, ' layers in the document' 
+        def __init__(self, position,image):
+            pygame=Player.pygame
+            # Call pygame.sprite.Sprite.__init__ to do some internal work
+            pygame.sprite.Sprite.__init__(self)
+
+            # Load the stickMan
+            self.image = pygame.image.load(image).convert()
+
+            # Create a rectangle
+            self.rect = self.image.get_rect()
+
+            # Position the rectangle
+            self.rect.x = position[0]
+            self.rect.y = position[1]
+        
+    def draw(self):
+        pygame=self.pygame
+        sys=self.sys
+        pygame.init()
+        screen = pygame.display.set_mode((256, 256))
+        pygame.display.set_caption('Sprite Example')
+        screen.fill((159, 182, 205))
+
+        # Create a Avatar sprite
+        character = self.Avatar((screen.get_rect().x, screen.get_rect().y),
+                                                       self.data.view["avatar"])
+
+        # Blit the sprite
+        screen.blit(character.image, character.rect)
+
+        pygame.display.update()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+    
+
+test=Player('Player_demo.svg') 
+test.draw()
+   
+#layers=SVGParser()
+#parse(,layers)
+#print 'There are ', layers.layers_count, ' layers in the document' 
